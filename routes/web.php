8c01ee233e57d5380503ad5eecf45ab4f418bd1a
mohamed-jilani <119133181+mohamed-jilani@users.noreply.github.com>
2023-05-07 19:49:17 +0100

    Student AUTH

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\studentController;
+use App\Http\Controllers\CustomAuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CustomAuthController::class, 'home']);
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('postlogin', [CustomAuthController::class, 'login'])->name('postlogin');
+Route::get('signup', [CustomAuthController::class, 'signup'])->name('register-user');
+Route::post('postsignup', [CustomAuthController::class, 'signupsave'])->name('postsignup');
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 Route::resource('/student', studentController::class);
 Route::get('/students/create', [studentController::class,'create']);
